8c80f80d8d3097cec2b718faa47d508659bfb8ee
HichamGsb <hichamgsb.pro@gmail.com>
2023-09-20 14:14:41 +0200

    Modification de la classe de la directive (sans application)

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[appBorderCard]' // Directive d'attribut donc on met des crochets
+  selector: '[pkmnBorderCard]' // Directive d'attribut donc on met des crochets
 })
 export class BorderCardDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) {
+    this.setHeight(180);
+    this.setBorderColor('#f5f5f5');
+   }
 
+  setHeight(height: number) {
+    this.el.nativeElement.style.height = `${height}px`
+  }
+
+  setBorderColor(color: String) {
+    this.el.nativeElement.style.border = `solid 4x ${color}`;
+  }
 }
