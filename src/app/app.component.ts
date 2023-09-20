d2e42fe6992a3df60afb5e03f8d1f13a8b0855cf
HichamGsb <hichamgsb.pro@gmail.com>
2023-09-20 11:22:41 +0200

    Ajout des méthodes ngOnInit et selectPokemon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `<h1>Welcome to {{ title }}</h1>`
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  title = 'Pokemon App'
+  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+    this.selectPokemon('Bulbizarre')
+  }
+
+  selectPokemon(pokemonName: String) {
+    console.log(`Vous avez cliqué sur le pokemon${pokemonName}.`)
+  }
 }
