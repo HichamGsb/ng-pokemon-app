ca4367c2faa9e6814ff9d082228eaa1015ec171d
HichamGsb <hichamgsb.pro@gmail.com>
2023-09-20 15:08:18 +0200

    Pipe personnalisé : modification de la classe et de l'élément html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// @angular/compiler
 
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
