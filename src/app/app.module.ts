b6c239f052d41515af42e3ba5c2f4720b8cc4bee
HichamGsb <hichamgsb.pro@gmail.com>
2023-09-24 20:09:46 +0200

    Module : Factorisation du code dans un module 'pokemon'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 // @angular/compiler
 
 import { AppComponent } from './app.component';
-import { BorderCardDirective } from './border-card.directive';
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    PokemonModule,
     AppRoutingModule
   ],
   providers: [],
