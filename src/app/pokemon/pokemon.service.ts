f6c42639972f1909c5e9dbd15877abba00d6a3c0
HichamGsb <hichamgsb.pro@gmail.com>
2023-09-25 15:48:51 +0200

    HTTP : Factorisation (gestion des erreurs)

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Pokemon } from './pokemon';
-import { POKEMONS } from './mock-pokemon-list';
 import {HttpClient} from "@angular/common/http";
 import {catchError, Observable, of, tap} from "rxjs";
-import {error} from "@angular/compiler-cli/src/transformers/util";
+
 
 @Injectable()
 export class PokemonService {
@@ -15,23 +14,25 @@ export class PokemonService {
   getPokemonList(): Observable<Pokemon[]> {
     return this.http.get<Pokemon[]>('api/pokemons').pipe(
         tap((pokemonList) => console.table(pokemonList)),
-        catchError((error) => {
-          console.log(error);
-          return of([])
-        })
+        catchError((error) => this.handleError(error, []))
     );
   }
 
   getPokemonById(pokemonId: number): Observable<Pokemon|undefined> {
     return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
-        tap((pokemon) => console.log(pokemon)),
-        catchError((error) => {
-          console.log(error);
-          return of(undefined);
-        })
+        tap((response) => this.log(response)),
+        catchError((error) => this.handleError(error, undefined))
     );
   }
 
+  private log(response: Pokemon[]|Pokemon|undefined) {
+    console.table(response);
+  }
+
+  private handleError(error : Error, errorValue: any ) {
+    console.error(error);
+    return of(errorValue);
+  }
   getPokemonTypeList(): string[] {
     return [
       'Plante',
